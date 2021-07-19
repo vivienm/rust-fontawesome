e5fe7c8939dd9d8017c0531d98eb2cdac3714db5
vivienm <vivienm@users.noreply.github.com>
2021-07-19 16:05:13 +0000

    Deploying to gh-pages from @ vivienm/rust-fontawesome@3e67db5225429e37feadab89d51235fdb76c3ad3 🚀

---
+++ implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["fontawesome"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&lt;<a class=\"enum\" href=\"fontawesome/enum.Icon.html\" title=\"enum fontawesome::Icon\">Icon</a>&gt; for <a class=\"enum\" href=\"fontawesome/enum.Icon.html\" title=\"enum fontawesome::Icon\">Icon</a>","synthetic":false,"types":["fontawesome::Icon"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
