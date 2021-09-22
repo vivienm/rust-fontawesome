fa3e2dfd88717301cc0209ca7bb937d153cade2d
vivienm <vivienm@users.noreply.github.com>
2021-09-22 09:00:35 +0000

    Deploying to gh-pages from @ vivienm/rust-fontawesome@620a21ade25f2d34dc3165a5549301cf6511cb0b 🚀

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["fontawesome"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"fontawesome/struct.TryFromIconError.html\" title=\"struct fontawesome::TryFromIconError\">TryFromIconError</a>","synthetic":false,"types":["fontawesome::TryFromIconError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
