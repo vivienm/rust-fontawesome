ce078a3056ccaf7bb0fbe1843ac5064dc1c89343
vivienm <vivienm@users.noreply.github.com>
2022-03-10 10:53:44 +0000

    Deploying to gh-pages from @ vivienm/rust-fontawesome@cef7129ad9e3dfcbc225db73314e57528eee3771 🚀

--- implementors/core/str/traits/trait.FromStr.js
+++ implementors/core/str/traits/trait.FromStr.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["fontawesome"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"fontawesome/struct.Version.html\" title=\"struct fontawesome::Version\">Version</a>","synthetic":false,"types":["fontawesome::version::Version"]}];
+implementors["fontawesome"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"fontawesome/struct.Version.html\" title=\"struct fontawesome::Version\">Version</a>","synthetic":false,"types":["fontawesome::version::Version"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
